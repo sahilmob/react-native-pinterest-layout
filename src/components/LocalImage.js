05f86781f69804fe14cd5e20f491327fac36bd86
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-18 23:41:44 +0200

    add logic to respond to screen rotation

--- src/components/LocalImage.js
+++ src/components/LocalImage.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import { Image, StyleSheet, Dimensions } from "react-native";
 import { scale } from "react-native-size-matters";
+export default class LocalImage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			windowWidth: Dimensions.get("window").width
+		};
+		Dimensions.addEventListener("change", this.updateWidth);
+	}
+	componentWillUnmount() {
+		Dimensions.removeEventListener("change", this.updateWidth);
+	}
 
-export default (LocalImage = ({ source, originalWidth, originalHeight }) => {
-	let windowWidth = Dimensions.get("window").width;
-	let widthChange = (windowWidth - scale(32)) / originalWidth;
-	let newWidth = originalWidth * widthChange;
-	let newHeight = originalHeight * widthChange;
+	updateWidth = dims => {
+		this.setState({
+			windowWidth: dims.window.width
+		});
+	};
 
-	const styles = StyleSheet.create({
-		imgStyles: {
-			width: newWidth,
-			height: newHeight
-		}
-	});
+	render() {
+		const { source, originalWidth, originalHeight } = this.props;
+		let windowWidth = this.state.windowWidth;
+		let widthChange = (windowWidth - scale(32)) / originalWidth;
+		let newWidth = originalWidth * widthChange;
+		let newHeight = originalHeight * widthChange;
 
-	return <Image source={source} style={styles.imgStyles} />;
-});
+		const styles = StyleSheet.create({
+			imgStyles: {
+				width: newWidth,
+				height: newHeight
+			}
+		});
+
+		return <Image source={source} style={styles.imgStyles} />;
+	}
+}

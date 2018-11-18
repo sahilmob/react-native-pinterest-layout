e4ad7147157e9004580ee8f9cd6a5eebb3524c1c
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-18 16:23:43 +0200

    remove unused vars. and imports

--- App.js
+++ App.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, ScrollView, View } from "react-native";
+import { StyleSheet, ScrollView } from "react-native";
 
 import Pin from "./src/components/Pin";
-
-const instructions = Platform.select({
-	ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-	android:
-		"Double tap R on your keyboard to reload,\n" +
-		"Shake or press menu button for dev menu"
-});
 
 export default class App extends Component {
 	render() {
@@ -23,15 +16,5 @@ export default class App extends Component {
 const styles = StyleSheet.create({
 	container: {
 		backgroundColor: "#F5FCFF"
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: "center",
-		margin: 10
-	},
-	instructions: {
-		textAlign: "center",
-		color: "#333333",
-		marginBottom: 5
 	}
 });
